bdeec7ddc8d13474b3c78d7a7ee1d664b748aa3f
Tshweu Sephiri <t.t.sephiri@gmail.com>
2023-05-30 23:09:25 +0200

    added vehicle form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
+    UserFormComponent,
+    VehicleFormComponent
   ],
   imports: [
     BrowserModule,
